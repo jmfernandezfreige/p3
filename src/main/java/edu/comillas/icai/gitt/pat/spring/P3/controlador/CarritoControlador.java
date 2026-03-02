f81c131718d7e6b63b8d4b0b26ff1a8e70e0c05c
jmfernandezfreige <202311871@alu.comillas.edu>
2026-03-03 00:15:28 +0100

    Arreglo paquetes

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/P3/controlador/CarritoControlador.java
@@ -0,0 +1,8 @@
+package edu.comillas.icai.gitt.pat.spring.P3.controlador;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarritoControlador {
+
+}
