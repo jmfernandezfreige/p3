3c37cd6a40a0fe758159fd51adfec800eb51563e
jmfernandezfreige <202311871@alu.comillas.edu>
2026-03-03 01:21:08 +0100

    Logica carrito controlador

--- src/main/java/edu/comillas/icai/gitt/pat/spring/P3/controlador/CarritoControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/P3/controlador/CarritoControlador.java
@@ -1,19 +1,46 @@
 package edu.comillas.icai.gitt.pat.spring.P3.controlador;
 
+import edu.comillas.icai.gitt.pat.spring.P3.entidad.Carrito;
+import edu.comillas.icai.gitt.pat.spring.P3.entidad.Usuario;
 import edu.comillas.icai.gitt.pat.spring.P3.repositorio.RepoCarrito;
 import edu.comillas.icai.gitt.pat.spring.P3.repositorio.RepoLineadeCarrito;
 import edu.comillas.icai.gitt.pat.spring.P3.repositorio.RepoUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 public class CarritoControlador {
     @Autowired
-    RepoCarrito repoCarrito;
-    RepoLineadeCarrito repoLineadeCarrito;
-    RepoUsuario repoUsuario;
+    private RepoCarrito repoCarrito;
+    @Autowired
+    private RepoLineadeCarrito repoLineadeCarrito;
+    @Autowired
+    private RepoUsuario repoUsuario;
 
-    @PostMapping
+    @GetMapping("/api/carritos")
+    public Iterable<Carrito> getCarritos() {
+        return this.repoCarrito.findAll();
+    }
+    @PostMapping("/api/carritos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Carrito creaCarrito(@RequestBody Carrito carritoNuevo) {
+        this.repoCarrito.save(carritoNuevo);
+        return carritoNuevo;
+    }
 
+    @GetMapping("/api/carritos/{idCarrito}")
+    public Carrito getCarrito(@PathVariable Long idCarrito) {
+        return this.repoCarrito.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/api/carritos/{idCarrito}")
+    public void borraCarrito(@PathVariable Long idCarrito) {
+        Carrito carrito = this.repoCarrito.findById(idCarrito).orElse(null);
+        this.repoCarrito.delete(carrito);
+    }
+
+    //Linea de Carrito
 }
